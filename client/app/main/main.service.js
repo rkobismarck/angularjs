9a8c5c9fef098736ec86285d0c1493accbd0b555
rkobismarck <rko.bismarck@gmail.com>
2018-03-31 11:56:42 -0600

    Revert "Initial repo commit."
    This reverts commit f47c6a62755b1cd207b71bb93f7fbb1ce1c2b081.

--- client/app/main/main.service.js
+++
@@ -1,7 +0,0 @@
-angular.module('dashboardApp')
-    .service('mainService', function(recursive) {
-
-    const reverseString = (argument) => recursive.summarize(argument);
-
-    return {reverseString};
-  });
